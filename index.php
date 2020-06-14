cd0539e6ab4222830c9a7dad66068fddcb4aef3a
Felly ILUNGA <fellyunikin7@gmail.com>
2020-06-13 21:18:39 -0400

    initial commit

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Acueil</title>
+
+	<!--external css-->
+  <link href="style.css" rel="stylesheet" />
+</head>
+<body>
+
+	<header>
+		<!-- Logo Header -->
+		<div class="logo">
+			<span>Logo</span>
+		</div>
+
+		<!-- Bar de navigation -->
+		<nav class="nav-bar">
+			<li class="item-nav">Acueil</li>
+			<li class="item-nav">Galerie</li>
+			<li class="item-nav">Contact</li>
+		</nav>
+	</header>
+
+	<body>
+		
+		<div class="container">
+			<article>
+				<h2 class="title">Titre 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</article>
+			<article>
+				<h2 class="title">Titre 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</article>
+			<article>
+				<h2 class="title">Titre 1</h2>
+				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+				tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+				quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+				consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+				cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+				proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+			</article>
+		</div>
+
+	</body>
+
+</body>
+</html>
